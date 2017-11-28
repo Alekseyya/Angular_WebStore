e09a748ed4dbe62e68381c6ec3269c4ef9f42df5
Fokiruna <alekseyyarchuk@gmail.com>
2017-11-28 18:48:08 +0300

    First commit

---
+++ WebStore/src/app/Catalog/CatalogBoard/catalog-board.component.ts
@@ -0,0 +1,8 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'catalog-board',
+  templateUrl: './catalog-board.component.html',
+  styleUrls: ['./catalog-board.component.css']
+})
+export class CatalogBoard { }
---
+++ WebStore/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import {Routes, RouterModule} from '@angular/router';
+import { AppComponent } from './app.component';
+
+
+import { AboutComponent }   from './About/about.component';
+import { HomeComponent }   from './Home/home.component';
+import { NotFoundComponent }   from './Not-Found/not-found.component';
+import { HomeHeaderComponent }   from './Headers/home-header.component';
+
+import { MainMenuComponent }   from './MainMenu/main-menu.component';
+
+
+//catalog
+import { CatalogBoard }   from './Catalog/CatalogBoard/catalog-board.component';
+
+const appRoutes: Routes =[
+  { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,  AboutComponent, NotFoundComponent, MainMenuComponent,
+
+    HomeComponent,
+    HomeHeaderComponent,
+
+    CatalogBoard
+
+  ],
+  imports: [
+    BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
